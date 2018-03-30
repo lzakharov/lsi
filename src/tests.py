5ea9010705732fda807a286430b1b17748002b8d
Lev Zakharov <l.j.zakharov@gmail.com>
2018-03-31 00:31:48 +0300

    Add simple test

---
+++ src/tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+from .lsi import LSI
+
+
+class LSITest(unittest.TestCase):
+    def test(self):
+        documents = [
+            'Shipment of gold damaged in a fire.',
+            'Delivery of silver arrived in a silver truck.',
+            'Shipment of gold arrived in a truck.']
+        q = 'gold silver truck'
+        lsi = LSI(documents, q)
+        ranking = lsi.process()
+
+        self.assertTrue(all(ranking == [2, 3, 1]))
+
+
+if __name__ == '__main__':
+    unittest.main()
